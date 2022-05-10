e9913e043da838d378868e0d08ce391147edcc71
Patrick von Platen <patrick.v.platen@gmail.com>
2022-05-10 17:59:45 +0000

    push

--- megatron/initialize.py
+++ megatron/initialize.py
@@ -62,7 +62,7 @@ def initialize_megatron(extra_args_provider=None, args_defaults={},
         # Random seeds for reproducibility.
         if args.rank == 0:
             print('> setting random seeds to {} ...'.format(args.seed))
-        _set_random_seed(args.seed)
+#        _set_random_seed(args.seed)
 
     # Set pytorch JIT layer fusion options.
     _set_jit_fusion_options()
